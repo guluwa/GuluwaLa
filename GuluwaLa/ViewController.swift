c38f0f66a7df6ec9166e02c04169f2261b5772f3
杨俊康 <903414363@qq.com>
2021-08-30 14:57:27 +0800

    新建项目和UI库

--- GuluwaLa/ViewController.swift
+++ GuluwaLa/ViewController.swift
@@ -6,14 +6,12 @@
 //
 
 import UIKit
+import DesignKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(DesignKitTest.testStr)
     }
-
-
 }
-
---
+++ Pods/Target Support Files/Pods-GuluwaLa-GuluwaLaUITests/Pods-GuluwaLa-GuluwaLaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GuluwaLa_GuluwaLaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GuluwaLa_GuluwaLaUITests
+@end
